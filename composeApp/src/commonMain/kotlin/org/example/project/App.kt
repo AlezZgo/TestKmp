3ab60f8d43cd9b18eabce42c6ba06357aa019a36
glebov.aleksandr22 <glebov.aleksandr22@rwb.ru>
2026-02-08 16:45:14 +0300

    safeContentPadding

--- composeApp/src/commonMain/kotlin/org/example/project/App.kt
+++ composeApp/src/commonMain/kotlin/org/example/project/App.kt
@@ -3,6 +3,8 @@ package org.example.project
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.safeContentPadding
 import org.example.project.root.RootComponent
 import org.example.project.ui.RootContent
 
@@ -12,6 +14,11 @@ fun App(
     modifier: Modifier = Modifier,
 ) {
     MaterialTheme {
-        RootContent(component = root, modifier = modifier)
+        RootContent(
+            component = root,
+            modifier = modifier
+                .fillMaxSize()
+                .safeContentPadding(),
+        )
     }
 }
